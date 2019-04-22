b5449d38b2d784cc8453c2a6d7d9884c7086f7a6
Felipe McChicken <aa@outlook.com>
2019-04-22 17:00:26 +0200

    first commit of personal site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateProjectsComponent } from './components/create-projects/create-projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'about-me', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'create-project', component: CreateProjectsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: ErrorComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateProjectsComponent } from './components/create-projects/create-projects.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateProjectsComponent,
+    ErrorComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
